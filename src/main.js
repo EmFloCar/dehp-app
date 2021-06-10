15f6b8e193abb4d682390f6286f3b67004c77f3d
Emilio Flórez Carrascal <65036537+EmFloCar@users.noreply.github.com>
2021-06-10 00:47:14 -0500

    feat: dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+require('@/assets/main.scss');
 
 Vue.config.productionTip = false
 
